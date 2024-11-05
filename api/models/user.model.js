7dd4446352b9b7b555286e5603d61da9be9953ea
ciheupenn <cihe@seas.upenn.com>
2024-11-04 23:44:38 -0500

    update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://www.gravatar.com/avatar/?d=mp",
+        default: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/97/39/7f/caption.jpg?w=2400&h=-1&s=1&cx=1920&cy=1080&chk=v1_f31158e4bb953d28a308",
     },
 }, { timestamps: true });
 
